a0b1f89e5d31186d7beb4aad499a16aa7b379f02
Al1Bi2 <gggvvvv576@gmail.com>
2024-12-21 00:05:48 +0300

    init and hopefully final commit

---
+++ main.py
@@ -0,0 +1,81 @@
+from logging import exception
+
+from mutagen.easyid3 import EasyID3
+import logging
+from vkpymusic import  TokenReceiver,Service
+import dotenv
+import os
+import sys
+import argparse
+
+LOG_FORMAT = logging.Formatter(u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
+LOG_FILENAME = "bot.log"
+LOG_LEVEL = logging.WARN
+
+
+def _setup_logger() -> logging.Logger:
+    file_handler = logging.FileHandler(LOG_FILENAME,encoding="utf-8")
+    file_handler.setFormatter(LOG_FORMAT)
+    file_handler.setLevel(LOG_LEVEL)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(LOG_FORMAT)
+    console_handler.setLevel(LOG_LEVEL)
+
+    bot_logger = logging.getLogger('bot')
+    bot_logger.setLevel(LOG_LEVEL)
+    bot_logger.addHandler(file_handler)
+    bot_logger.addHandler(console_handler)
+    return bot_logger
+
+def load_env(path : str = None):
+    dotenv.load_dotenv()
+    env = dotenv.dotenv_values()
+    return env
+
+def search(query, count = 10):
+    env = load_env()
+    service = Service.parse_config()
+    service.set_logger(_setup_logger())
+    if  service is None or not service.is_token_valid():
+        token_reciever = TokenReceiver(env["VK_LOGIN"],env["VK_PASS"])
+        if token_reciever.auth():
+            token_reciever.get_token()
+            token_reciever.save_to_config()
+        else:
+            print("Check your credentials")
+            exit(-1)
+        service = Service.parse_config()
+
+    songs = service.search_songs_by_text(query,count = count)
+    if songs is None:
+        print("No songs :(")
+        exit(-2)
+    for i,song in enumerate(songs,1):
+        print(f"""{i}) {song.title}—{song.artist} ({song.duration//60}:{song.duration%60:02})""")
+        print(song.artist)
+
+
+    n = int(input("Enter num of song: "))-1
+    song = songs[n]
+    song.to_safe()
+    title  = song.title
+    artist = song.artist
+    temp_audio_path = service.save_music(song)
+    audio = EasyID3(temp_audio_path)
+    audio["title"] = title
+    audio["artist"] = artist
+    audio.save()
+    os.replace(temp_audio_path,f"{env['PATH']}{title}—{artist}.mp3")
+
+
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query",nargs = "+", default = "test_music",type=str)
+    parser.add_argument("-n", nargs=1, default="10", type=int)
+    args = parser.parse_args()
+
+    query_str = " ".join(args.query)
+    search(query_str,args.n)
